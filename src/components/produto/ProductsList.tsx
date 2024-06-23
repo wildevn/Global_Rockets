2f32096b2c95ce7259dc4ec08eb09b370c38776f
wildevn <45374307+wildevn@users.noreply.github.com>
2024-06-23 20:36:07 -0300

    Update ProductsList.tsx

--- src/components/produto/ProductsList.tsx
+++ src/components/produto/ProductsList.tsx
@@ -1,24 +1,56 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import ProductItem from './ProductItem'
 import { chevronLeftIcon, chevronRightIcon } from '../icons'
 
 export default function ProductsList() {
+    const [scrollbarPos, setScrollbarPos] = useState(150)
+
+    function onHandleScroll(increment: boolean) {
+        if(increment && scrollbarPos < 3344) {
+            setScrollbarPos(scrollbarPos + 176)
+        }
+        else if(!increment && scrollbarPos > -3000) {
+            setScrollbarPos(scrollbarPos - 176)
+        }
+    }
+
     return (
-        <div className="w-full h-72 flex items-center overflow-x-hidden">
+        <div className="w-full h-72 flex items-center overflow-x-hidden pt-4 mb-6">
             <div className='w-[5%]'>
-                {chevronLeftIcon()}
+                <span onClick={() => onHandleScroll(true)} className='w-full flex items-center justify-center'>
+                    {chevronLeftIcon()}
+                </span>
             </div>
-            <div className="w-[90%] flex flex-nowrap flex-grow-0 flex-shrink-0 overflow-x-hidden h-full no-scrollbar">
-                <ProductItem area='default'/>
-                <ProductItem area='default'/>
-                <ProductItem area='default'/>
-                <ProductItem area='default'/>
-                <ProductItem area='default'/>
-                <ProductItem area='default'/>
+            <div className="w-[90%] h-full overflow-x-hidden">
+                <div className={`flex flex-nowrap flex-grow-0 flex-shrink-0 h-full translate-x-[${scrollbarPos}px]`}>
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    <ProductItem area='default' />
+                    
+                </div>
             </div>
             <div className='w-[5%]'>
-                {chevronRightIcon()}
+                <span onClick={() => onHandleScroll(false)} className='w-full flex items-center justify-center'>
+                    {chevronRightIcon()}
+                </span>
             </div>
         </div>
     )
